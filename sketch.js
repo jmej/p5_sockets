76330f39f6246d83cb643449b5293b30f7f313b0
jmej <jmejia@anestheticaudio.com>
2019-02-25 10:40:27 -0800

    'fixups'

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:8080');
+  socket = io.connect('http://localhost:8000');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
