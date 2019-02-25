7a4fd155a9a3f17cc0efa94f518a6801d2660b0c
jmej <jmejia@anestheticaudio.com>
2019-02-25 10:32:30 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+// The file system module
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+let server = http.createServer(handleRequest);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+server.listen(port);
+
+function handleRequest(req, res) {
+  // What did we request?
+  let pathname = req.url;
+  
+  // If blank let's ask for index.html
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+  
+  // Ok what's our file extension
+  let ext = path.extname(pathname);
+
+  // Map extension to file type
+  const typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+
+  // What is it?  Default to plain text
+  let contentType = typeExt[ext] || 'text/plain';
+
+  // Now read and write back the file with the appropriate content type
+  fs.readFile(__dirname + pathname,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      // Dynamically setting content type
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function (socket) {
+  
+    console.log("We have a new client: " + socket.id);
+  
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+      
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+        
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+    
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
+  }
+);
+
+
