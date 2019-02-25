d31d8518806c22ec3e645754e7fdc1cdba506e41
jmej <jmejia@anestheticaudio.com>
2019-02-25 10:44:06 -0800

    changes url for heroku

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:8000');
+  socket = io.connect('https://socket-hack.herokuapp.com/');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
